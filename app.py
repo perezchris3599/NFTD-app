386d6140c023457b78154eeabad351484860e07a
perezchris3599 <kweeziperez712@gmail.com>
2022-07-13 19:33:56 +0300

    added class

--- app.py
+++ app.py
@@ -2,14 +2,22 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+
+
 @app.route('/')
-@app.route('/base')
 def index():
+
+    todo_list = Todo
+    print(todo_list)
     return render_template('base.html')
 
 @app.route('/about')
@@ -18,4 +26,9 @@ def about():
 
 if __name__ == "__main__":
     db.create_all()
+
+    new_todo = Todo(title="todo 1", complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+
     app.run(debug=True)
